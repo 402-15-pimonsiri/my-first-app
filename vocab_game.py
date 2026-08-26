ad201b2a4f50f2bd3afc1635efca237562a8470f
402-15-pimonsiri <54245@yupparaj.ac.th>
2026-08-26 14:53:24 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -20,8 +20,12 @@ def reset_game():
     st.session_state.start = time.time() 
     st.session_state.is_ended = False 
 
+
+# ----------------------------------------------------
+# 📌 ฟังก์ชัน MessageBox (Dialog)
+# ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2, ans3, ans4):
+def show_result_dialog(ans1, ans2):
     st.balloons()
     score = 0
 
@@ -30,12 +34,12 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     u_ans3 = ans3.strip().lower()
     u_ans4 = ans4.strip().lower()
 
- if u_ans1 == "Cherry":
+ if u_ans1 == "apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
    else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
- if u_ans2 == "Peach":
+ if u_ans2 == "fish":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
     else:
@@ -58,6 +62,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     else:
         st.error("💀 You lose!")
 
+
 st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
 
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
@@ -71,16 +76,17 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 st.divider()
 
+
 ans1 = st.text_input(
-    "ข้อ 1: Red, but not a strawberry—it's a `C _ e _ r y`! . 🍒",
+    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
     value=st.session_state.ans1_val,
 )
 ans2 = st.text_input(
-    "ข้อ 2: Soft, sweet, and fuzzy... guess what? I'm a `P _ _ c h `. 🍑",
+    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 3: `A v _ _a d o`is green inside and out, with a seed too, wink! .🥑 ",
+    "ข้อ 3: `A v _ _a d o`is green inside and out, with a seed too, wink! . 🥑 ",
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
